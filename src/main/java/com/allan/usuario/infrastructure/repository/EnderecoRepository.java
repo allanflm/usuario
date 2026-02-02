c7c94c7d2c605b2ba00a84865b58332d768e2279
Allan <allanfelipedk@gmail.com>
2026-02-02 13:47:09 -0300

    feat: selected classes of security

---
+++ src/main/java/com/allan/usuario/infrastructure/repository/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.allan.usuario.infrastructure.repository;
+
+import com.allanfelipe.aprendendospring.infrastructure.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface EnderecoRepository  extends JpaRepository<Endereco, Long> {
+
+}
