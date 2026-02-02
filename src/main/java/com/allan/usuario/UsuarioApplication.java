69d0c10d1a5a4267f75ecf4f31b3ee4306ede20e
Allan <allanfelipedk@gmail.com>
2026-02-02 13:19:33 -0300

    initial commit

---
+++ src/main/java/com/allan/usuario/UsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.allan.usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuarioApplication.class, args);
+	}
+
+}
