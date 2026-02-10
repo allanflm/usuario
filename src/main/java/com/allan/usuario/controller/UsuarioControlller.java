8b205513cc02756842f1ab399c03ce30c9bf9e0e
Allan <allanfelipedk@gmail.com>
2026-02-10 14:19:50 -0300

    feat: Added address and phone number.

--- src/main/java/com/allan/usuario/controller/UsuarioControlller.java
+++ src/main/java/com/allan/usuario/controller/UsuarioControlller.java
@@ -1,6 +1,8 @@
 package com.allan.usuario.controller;
 
 import com.allan.usuario.business.UsuarioService;
+import com.allan.usuario.business.dto.EnderecoDTO;
+import com.allan.usuario.business.dto.TelefoneDTO;
 import com.allan.usuario.business.dto.UsuarioDTO;
 import com.allan.usuario.infrastructure.entity.Usuario;
 import com.allan.usuario.infrastructure.security.JwtUtil;
@@ -38,7 +40,7 @@ public class UsuarioControlller {
     }
 
     @GetMapping
-    public ResponseEntity<Usuario> buscarUsuarioPorEmail(@RequestParam("email") String email){
+    public ResponseEntity<UsuarioDTO> buscarUsuarioPorEmail(@RequestParam("email") String email){
         return ResponseEntity.ok(usuarioService.buscarUsuarioPorEmail(email));
     }
 
@@ -54,4 +56,18 @@ public class UsuarioControlller {
 
         return ResponseEntity.ok(usuarioService.atualizaDadosUsuario(token, dto));
     }
+
+    @PutMapping("/endereco")
+    public ResponseEntity<EnderecoDTO> atualizaEnderecoUsuario(@RequestBody EnderecoDTO enderecoDTO,
+                                                           @RequestParam("id") Long id){
+
+        return ResponseEntity.ok(usuarioService.atualizaEndereco(id, enderecoDTO));
+    }
+    @PutMapping("/telefone")
+    public ResponseEntity<TelefoneDTO> atualizaEnderecoUsuario(@RequestBody TelefoneDTO telefoneDTO,
+                                                               @RequestParam("id") Long id){
+
+        return ResponseEntity.ok(usuarioService.atualizaTelefone(id, telefoneDTO));
+    }
+
 }
